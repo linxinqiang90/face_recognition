e562785354876194f541396d336f7824e10ce540
linxinqiang <linxinqiang@xrjiot.com>
2023-09-04 14:53:20 +0800

    update

--- test3.py
+++ test3.py
@@ -42,13 +42,16 @@ while True:
     # Grab a single frame of video
     ret, frame = video_capture.read()
 
+    # cv2.imshow("test",frame)
+    # cv2.waitKey(30)
     # Only process every other frame of video to save time
     if process_this_frame:
         # Resize frame of video to 1/4 size for faster face recognition processing
         small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
         # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-        rgb_small_frame = small_frame[:, :, ::-1]
+        # rgb_small_frame = small_frame[:, :, ::-1]
+        rgb_small_frame = np.ascontiguousarray(small_frame[:, :, ::-1])
 
         # Find all the faces and face encodings in the current frame of video
         face_locations = face_recognition.face_locations(rgb_small_frame)
